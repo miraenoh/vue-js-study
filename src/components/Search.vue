1ecf83ddec85fb7d0d6be7ab8f2a376028695ca8
miraenoh <mnoh@novamobility.com>
2020-03-06 14:08:20 -0800

    Implemented a primitive app with search input

---
+++ src/components/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input v-model="searchQuery" @keyup.enter="search" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+  data () {
+    return {
+      searchQuery: ''
+    }
+  },
+  methods: {
+    search () {
+      alert('search')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
