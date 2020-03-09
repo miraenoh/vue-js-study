6720483a568c43213d74724fc7b73a4682a777b6
miraenoh <mnoh@novamobility.com>
2020-03-09 13:22:06 -0700

    Created and implement the MovieList component and the test

---
+++ src/components/MovieList.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <movie-list-item
+      v-for="movie in movies"
+      :movie="movie"
+      :key="movie.imdbID"
+    />
+  </ul>
+</template>
+
+<script>
+import MovieListItem from '../components/MovieListItem'
+
+export default {
+  name: 'movie-list',
+  components: {
+    MovieListItem
+  },
+  props: {
+    movies: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style></style>
