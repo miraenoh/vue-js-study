5176903f8bdc84739b43bf80eb305f6fcb9383f6
Mirae Noh <mnoh@novamobility.com>
2020-04-18 18:07:28 +0900

    Implemented logs

--- 1-the-monster-slayer/app.js
+++ 1-the-monster-slayer/app.js
@@ -3,16 +3,25 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function() {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
+
             if (this.checkFinished()) {
                 return;
             }
@@ -20,7 +29,14 @@ new Vue({
             this.monsterAttacks();
         },
         specialAttack: function() {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
+            
             if (this.checkFinished()) {
                 return;
             }
@@ -33,14 +49,27 @@ new Vue({
             } else {
                 this.playerHealth = 100;
             }
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
+
             this.monsterAttacks();
         },
         giveUp: function() {
             this.gameIsRunning = false;
         },
         monsterAttacks: function() {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+
             this.checkFinished();
+
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
