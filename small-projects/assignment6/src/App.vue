f3b1f0b09177d6c99f0ef4580325cd1ef9406713
miraenoh <yes_future@naver.com>
2020-04-19 23:32:11 +0900

    Finished the assignment6

---
+++ small-projects/assignment6/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <my-header />
+    <hr />
+    <div class="row">
+      <my-server-list />
+      <my-server-details />
+    </div>
+    <hr />
+    <my-footer />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import ServerList from "./components/ServerList.vue";
+import ServerDetails from "./components/ServerDetails.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    "my-header": Header,
+    "my-server-list": ServerList,
+    "my-server-details": ServerDetails,
+    "my-footer": Footer
+  }
+};
+</script>
+
+<style>
+</style>
