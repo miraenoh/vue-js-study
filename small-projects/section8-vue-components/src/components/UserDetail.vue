28451a27bdab36ccac0be7d67b8ec794c0d8b523
miraenoh <yes_future@naver.com>
2020-04-20 00:52:49 +0900

    Added a property for communication

--- small-projects/section8-vue-components/src/components/UserDetail.vue
+++ small-projects/section8-vue-components/src/components/UserDetail.vue
@@ -1,15 +1,34 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-    </div>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ name }}</p>
+    <p>Reversed Name: {{ switchName() }}</p>
+  </div>
 </template>
 
 <script>
+export default {
+//   props: ['name'], // props as an array
+  props: {
+/*       name: String
+    //   name: [String, Array]  // We can validate multiple types like this */
+    name: {
+        type: String,
+        required: true,
+        default: "anonymous",
+    }
+  },
+  methods: {
+      switchName() {
+          return this.name.split("").reverse().join("");
+      }
+  }
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+div {
+  background-color: lightcoral;
+}
 </style>
