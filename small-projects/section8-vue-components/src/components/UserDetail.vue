cfb6f4c24de451e5aee74cefdf98bf9b4ac35afe
miraenoh <yes_future@naver.com>
2020-04-20 01:02:35 +0900

    Added a custom event for communication

--- small-projects/section8-vue-components/src/components/UserDetail.vue
+++ small-projects/section8-vue-components/src/components/UserDetail.vue
@@ -4,25 +4,34 @@
     <p>Many Details</p>
     <p>User Name: {{ name }}</p>
     <p>Reversed Name: {{ switchName() }}</p>
+    <button @click="resetName">Reset Name</button>
   </div>
 </template>
 
 <script>
 export default {
-//   props: ['name'], // props as an array
+  //   props: ['name'], // props as an array
   props: {
-/*       name: String
+    /*       name: String
     //   name: [String, Array]  // We can validate multiple types like this */
     name: {
-        type: String,
-        required: true,
-        default: "anonymous",
+      type: String,
+      required: true,
+      default: "anonymous"
     }
   },
   methods: {
-      switchName() {
-          return this.name.split("").reverse().join("");
-      }
+    switchName() {
+      return this.name
+        .split("")
+        .reverse()
+        .join("");
+    },
+    resetName() {
+      this.name = "Max";
+      //   Emit a cumtom event
+      this.$emit("nameWasReset", this.name);
+    }
   }
 };
 </script>
