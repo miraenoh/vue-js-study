27a06d8d39bc244974efd7227a07b0ec5a3e082e
miraenoh <yes_future@naver.com>
2020-04-20 18:09:54 +0900

    Added slots for passing content

--- small-projects/section9-components-advanced/src/App.vue
+++ small-projects/section9-components-advanced/src/App.vue
@@ -2,21 +2,29 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <app-quote />
+        <app-quote>
+          <h2 slot="title">{{quoteTitle}}</h2>
+          <p>A wonderful Quote!</p>
+        </app-quote>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Quote from './components/Quote.vue';
+import Quote from "./components/Quote.vue";
 
 export default {
+  data: function() {
+    return {
+      quoteTitle: "The Quote"
+    };
+  },
   components: {
     appQuote: Quote
   }
 };
 </script>
 
-<style>
+<style scoped>
 </style>
--- small-projects/section9-components-advanced/src/components/Quote.vue
+++ small-projects/section9-components-advanced/src/components/Quote.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <p>A wonderful Quote!</p>
+    <div class="title">
+      <slot name="title" />
+      <span style="color: #ccc">
+        <slot name="subtitle">The Subtitle</slot>
+      </span>
+    </div>
+    <div>
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -14,7 +22,14 @@ div {
   box-shadow: 1px 1px 2px black;
   padding: 30px;
   margin: 30px auto;
-  text-align: center
-  ;
+  text-align: center;
+}
+
+h2 {
+  color: red;
+}
+
+.title {
+  font-style: italic;
 }
 </style>
