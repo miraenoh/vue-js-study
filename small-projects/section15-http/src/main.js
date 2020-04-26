4f7a45cac975730382315984f5d1c904b69733f5
miraenoh <yes_future@naver.com>
2020-04-26 23:36:53 +0900

    Added a interceptor for requese & response

--- small-projects/section15-http/src/main.js
+++ small-projects/section15-http/src/main.js
@@ -8,6 +8,19 @@ Vue.use(VueResource);
 
 // $ is not required because here's "main.js"
 Vue.http.options.root = "https://vuejs-http-44327.firebaseio.com/data.json";
+// Intercept a request and do something
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == "POST") {
+    request.method = "PUT";
+  }
+  // intercepting response
+  next((response) => {
+    response.json = () => {
+      return { messages: response.body };
+    };
+  });
+});
 
 new Vue({
   render: (h) => h(App),
